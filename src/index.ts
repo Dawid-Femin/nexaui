ff827220ed46f5a8f8b11726e03eb2f03d019d1e
Dawid-Femin <d.femin@wp.pl>
2025-11-13 00:02:50 +0100

    feat: improve path in export and add components into main export file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { Alert } from "./MuiComponents/Alert/Alert";
+export { Breadcrumbs } from "./MuiComponents/Breadcrumbs/Breadcrumbs";
+export { Button } from "./MuiComponents/Button/Button";
+export { default as CropperComponent } from "./Components/Cropper";
 
